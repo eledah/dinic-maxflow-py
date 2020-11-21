2d088aa32c3665a6c3499d0d093d51b2187c626a
eledah <eledah@users.noreply.github.com>
2020-11-21 13:44:06 +0330

    Visualization

--- dinic_conv_n.py
+++ dinic_conv_n.py
@@ -1,15 +1,27 @@
 import xlrd
+import networkx as nx
+import matplotlib.pyplot as plt
 from queue import Queue
 from recordtype import recordtype
 
+plt.figure(figsize=(50, 50))
+
 Edge = recordtype('Edge', ['nextNode', 'reverseIndex', 'flow', 'cap'])
+
+G = nx.DiGraph()
+G_color = []
+G_edgeLabel = []
+G_Final = nx.DiGraph()
 
 nextNodeIndex = list()
 level = list()
 MAXVAL = 1000000000
 
 LOG_STEPS = True
-FILE_LOCATION = "C:/Users/eledah/Documents/Uni/tpmaxflow-py/data/d4.xlsx"
+DRAW_INITIAL_GRAPH = True
+DRAW_FINAL_GRAPH = True
+
+FILE_LOCATION = "C:/Users/eledah/Documents/Uni/tpmaxflow-py/data/d1.xlsx"
 
 
 def step_log(txt):
@@ -25,7 +37,7 @@ def addEdge(prevNode, nextNode, weight, graph):
     graph[nextNode].append(backward)
 
 
-def bfs(srcNode, destNode, graph):
+def bfs(srcNode, destNode, graph, G_Final):
     for index in range(len(level)):
         level[index] = -1
 
@@ -47,7 +59,7 @@ def bfs(srcNode, destNode, graph):
     return level[destNode] != -1
 
 
-def dfs(currNode, destNode, currMinFlow, graph):
+def dfs(currNode, destNode, currMinFlow, graph, G_Final):
     if currNode == destNode:
         return currMinFlow
 
@@ -57,28 +69,30 @@ def dfs(currNode, destNode, currMinFlow, graph):
 
         if level[e.nextNode] == level[currNode] + 1 and e.flow < e.cap:
             nextFlow = min(currMinFlow, e.cap - e.flow)
-            dfsVal = dfs(e.nextNode, destNode, nextFlow, graph)
+            dfsVal = dfs(e.nextNode, destNode, nextFlow, graph, G_Final)
 
             if dfsVal > 0:
                 e.flow += dfsVal
                 graph[e.nextNode][e.reverseIndex].flow -= dfsVal
+                G_Final.add_edge(e.reverseIndex + 1, e.nextNode + 1, weight=e.flow)
                 return dfsVal
 
     return 0
 
 
-def maxFlow(src, dest, graph):
+def maxFlow(src, dest, graph, G_Final):
     totalFlow = 0
 
-    while bfs(src, dest, graph):
+    while bfs(src, dest, graph, G_Final):
         for index in range(len(nextNodeIndex)):
             nextNodeIndex[index] = 0
-        flow = dfs(src, dest, MAXVAL, graph)
+        flow = dfs(src, dest, MAXVAL, graph, G_Final)
         while flow:
             totalFlow += flow
-            flow = dfs(src, dest, MAXVAL, graph)
+            flow = dfs(src, dest, MAXVAL, graph, G_Final)
 
     return totalFlow
+
 
 graph = list()
 step_log("Adding edges.")
@@ -134,17 +148,70 @@ for i in range(0, numNodes):
 for i in range(0, numEdges):
     graph.append(list())
 
+
 for i in range(1, sheet.nrows):
     src = int(sheet.row_values(i)[0] - 1)
     dest = int(sheet.row_values(i)[1] - 1)
     weight = int(sheet.row_values(i)[2])
     addEdge(src, dest, weight, graph)
 
+    # G.add_edge(src + 1, dest + 1)
+    if DRAW_INITIAL_GRAPH:
+        G.add_edge(src + 1, dest + 1, weight=weight)
+
+if DRAW_INITIAL_GRAPH:
+    for node in G:
+        if node == 1:
+            G_color.append('red')
+        elif node == numNodes:
+            G_color.append('green')
+        else:
+            G_color.append('blue')
+    edges = G.edges()
+    pos = nx.circular_layout(G)
+
+    nx.draw(G,
+            pos,
+            edges=edges,
+            alpha=0.9,
+            node_color=G_color,
+            with_labels=True
+            )
+    G_edgeLabel = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=G_edgeLabel)
+    plt.show()
+
+
 step_log("All edges added.")
 step_log("Starting calculations.")
 
-MAXFLOW = maxFlow(0, numNodes - 1, graph)
+MAXFLOW = maxFlow(0, numNodes - 1, graph, G_Final)
 
 step_log("Calculations done!")
 
 print("Maximum Possible Flow: ", MAXFLOW)
+
+
+if DRAW_FINAL_GRAPH:
+    plt.figure(figsize=(30, 30))
+    edges = G_Final.edges()
+    pos = nx.circular_layout(G_Final)
+    G_color = []
+    for node in G_Final:
+        if node == 1:
+            G_color.append('red')
+        elif node == numNodes:
+            G_color.append('green')
+        else:
+            G_color.append('blue')
+    nx.draw(G_Final,
+            pos,
+            edges=edges,
+            alpha=0.9,
+            node_color=G_color,
+            with_labels=True
+            )
+    G_edgeLabel = nx.get_edge_attributes(G_Final, 'weight')
+    nx.draw_networkx_edge_labels(G_Final, pos, edge_labels=G_edgeLabel)
+
+    plt.show()
